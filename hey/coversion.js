7dee55c702b7fbd10c0e5e230edc83978c315d1d
sonammishra2406-bit <sonammishra2406@gmail.com>
2026-06-11 09:07:28 +0000

    coversion.js

---
+++ hey/coversion.js
@@ -0,0 +1,12 @@
+let score="33a"
+let bul= 1;//bul= "sonam"
+let b=Boolean(bul);
+console.log(b);//true//sonam-->true
+console.log(typeof score);
+console.log(typeof (score));//string
+let a= Number(score);
+//console.log(a)
+console.log(typeof a)//number
+console.log(a)
+//"33"-->number
+//"33a"--> NaN( while printing a)
