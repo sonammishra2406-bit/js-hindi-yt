ded822fbcaca34f143b1294998ed0261d22723d5
sonammishra2406-bit <sonammishra2406@gmail.com>
2026-06-11 08:47:03 +0000

    var

---
+++ hey/var.js
@@ -0,0 +1,11 @@
+const Id= 1234;
+let email= "Sonam@gmail.com";
+var name="Sonam";
+let hi;
+Title= "Mishra"
+//alert(3+3) gives pop up of alert with answer 6
+/* shut
+null --> object; undefine-->undefined;Id-->number
+up*/
+let hey= false;
+console.table([Id, email, name, Title, hi]);
